125ef8d7b74ab728d233395695ec7732e965b034
samkamiru <samuel.kamiru@gmail.com>
2023-09-18 16:19:17 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './App.module.css'
+
+const MyApp = () => (
+    <div className={classes.container}>
+        <iframe title="IFBS Dashboard Report_v4" src="https://app.powerbi.com/view?r=eyJrIjoiNDlkNTU4MDItMjFkNy00MjUzLTk4NzMtYmVmNjA3YTA0YzBiIiwidCI6ImE4MmQ5Y2U3LTcwZDQtNGMzOS1iMmUyLTU5ZmQwODA0NjQ1YiIsImMiOjh9&pageName=ReportSection8d89abb2b5b0beab7abd" frameborder="0" allowFullScreen="true"></iframe>
+    </div>
+)
+
+export default MyApp
